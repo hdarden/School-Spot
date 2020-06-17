1cab98c0ef401d89ab3a2eede0b4bae15b3befd3
Ashley DeYoung <ashley.d.deyoung@gmail.com>
2020-06-17 07:21:10 -0400

    added on click for 'Enter Grade' button

--- public/js/teacher.js
+++ public/js/teacher.js
@@ -1,8 +1,12 @@
-$(document).ready(function() {
+/* eslint-disable no-mixed-spaces-and-tabs */
+$(document).ready(function () {
 	console.log("loaded");
 
 	var date_input = $("input[name=\"date\"]"); //our date input has the name "date"
-	var container = $(".bootstrap-iso form").length > 0 ? $(".bootstrap-iso form").parent() : "body";
+	var container =
+    $(".bootstrap-iso form").length > 0
+    	? $(".bootstrap-iso form").parent()
+    	: "body";
 	var options = {
 		format: "mm/dd/yyyy",
 		container: container,
@@ -16,20 +20,17 @@ $(document).ready(function() {
 	var taskText = $("#taskText");
 	var dateInput = $("#date");
 
-	
-
 	// When the signup button is clicked, we create taskData
-	newTaskForm.on("submit", function(event) {
+	newTaskForm.on("submit", function (event) {
 		console.log(dateInput.val());
 
 		event.preventDefault();
 		var taskData = {
 			taskDetails: taskText.val().trim(),
-			dueDate: dateInput.val()
+			dueDate: dateInput.val(),
 		};
 
 		console.log(taskData);
-
 
 		// If we have a text and a date, run the postTask function
 		postTask(taskData.taskDetails, taskData.dueDate);
@@ -46,19 +47,28 @@ $(document).ready(function() {
 		$.post("/api/addTask", {
 			taskDetails: text,
 			dueDate: date,
-
-		})
-			.then(function(data) {
-				console.log(data)
-				// If there's an error, handle it by throwing up a bootstrap alert
-			});
+		}).then(function (data) {
+			console.log(data);
+			// If there's an error, handle it by throwing up a bootstrap alert
+		});
 		// .catch(handleLoginErr);
 	}
+
+	$(".enterGrade").on("click", function (event) {
+		var id = $(this).data("id");
+
+		// Send the PUT request.
+		$.ajax("/api/addTask" + id, {
+			type: "PUT",
+		}).then(function () {
+			console.log("Grade Entered!");
+			// Reload the page to get the updated list
+			location.reload();
+		});
+	});
 
 	function handleLoginErr(err) {
 		$("#alert .msg").text(err.responseJSON);
 		$("#alert").fadeIn(500);
 	}
 });
-
-
